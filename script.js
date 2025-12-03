7b4b9decba2e60f72c99ddbf65a9c232acd171b5
Seneth <hi@seneth.me>
2025-12-03 18:29:50 -0500

    added functionality to the calculator

--- script.js
+++ script.js
@@ -424,3 +424,30 @@ interact('#calculator')
         calculator.classList.add('hidden');
     }); 
 });
+
+//calculator
+
+let display = document.getElementById('display');
+let buttons = Array.from(document.querySelectorAll('.calc-btn'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            case '=':
+                try {
+                    display.innerText = Function('"use strict"; return (' + display.innerText + ')')();
+                } catch {
+                    display.innerText = "Error";
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
